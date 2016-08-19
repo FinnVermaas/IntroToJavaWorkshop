5130975f66bf72af7e01fa7590af798b816066bb
FinnVermaas <league@Mac-mini.local>
2016-08-18 18:27:50 -0700

    I am making a change calculator

---
+++ src/changeCalculator.java
@@ -0,0 +1,31 @@
+
+
+// Copyright The League of Amazing Programmers 2014
+import javax.swing.JOptionPane;
+
+/*
+ * I have a pocket full of change. I hate doing math. Make me a program that
+ * will calculate how much money I have without me having to use my brain. Then
+ * make me a sandwich.
+ */
+public class changeCalculator {
+
+	public static void main(String[] args) {
+
+		// Ask the user how many nickels they havZ("how many nickels do you have");
+		String nickels =	JOptionPane.showInputDialog(null, "how many nickels do you have");
+		// Convert their answer to an int using Integer.parseInt()
+int nickelsNumber = Integer.parseInt(nickels);
+		// Ask the user how many dimes they have, and convert their answer
+String dimes = JOptionPane.showInputDialog(null, "how many dimes do you have");
+int dimesNumber = Integer.parseInt(dimes);
+		// Ask the user how many quarters they have, and convert their answer
+
+		// Calculate how much money the user has and save it in a double variable 
+
+		// Tell the user how much money they have
+
+	}
+}
+
+
